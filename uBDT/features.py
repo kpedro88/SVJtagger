e16b7c080a5710f7b6a372de419828f0de8c6743
Kevin Pedro <kpedro88@gmail.com>
2018-10-09 15:55:46 -0500

    central location of feature lists

---
+++ uBDT/features.py
@@ -0,0 +1,6 @@
+# define features
+uniform_features = ["pt"]
+train_features = ["mult","axisminor","girth","tau21","tau32","msd","deltaphi"]
+spectators = ["mt","eta"]
+all_vars = uniform_features+train_features+spectators
+
