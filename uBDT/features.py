8e1036bf2f8722e378c31ac0b77f22b469c17715
Kevin Pedro <kpedro88@gmail.com>
2018-10-12 13:43:19 -0500

    remove axisminor

--- uBDT/features.py
+++ uBDT/features.py
@@ -1,6 +1,6 @@
 # define features
 uniform_features = ["pt"]
-train_features = ["mult","axisminor","girth","tau21","tau32","msd","deltaphi"]
+train_features = ["mult","girth","tau21","tau32","msd","deltaphi"]
 spectators = ["mt","eta"]
 all_vars = uniform_features+train_features+spectators
 
