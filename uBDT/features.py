0b1c1d6cf068d3284dd6e8cd0048fc652342bbe6
Kevin Pedro <kpedro88@gmail.com>
2019-02-04 22:42:21 -0600

    remove old file

--- uBDT/features.py
+++
@@ -1,6 +0,0 @@
-# define features
-uniform_features = ["pt"]
-train_features = ["mult","girth","tau21","tau32","msd","deltaphi"]
-spectators = ["mt","eta"]
-all_vars = uniform_features+train_features+spectators
-
