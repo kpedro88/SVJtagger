3337f7e2006a11011e5b813ff545f55c03941032
Kevin Pedro <kpedro88@gmail.com>
2019-04-21 01:51:58 -0500

    disable errant print statement

--- uBDT/configs/D23.py
+++ uBDT/configs/D23.py
@@ -8,5 +8,5 @@ uconfig.dataset.signal = [ "SVJ_mZprime-"+str(mZprime)+"_mDark-20_rinv-0.3_alpha
                        + [ "SVJ_mZprime-3000_mDark-20_rinv-"+str(rinv)+"_alpha-peak_MC2017" for rinv in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8] ] \
                        + [ "SVJ_mZprime-3000_mDark-20_rinv-0.3_alpha-"+alpha+"_MC2017" for alpha in ["peak","high","low"] ]
 uconfig.dataset.signal = list(sorted(set(uconfig.dataset.signal)))
-print len(uconfig.dataset.signal)
+#print len(uconfig.dataset.signal)
 uconfig.features.spectator.extend(["mZprime","mDark","rinv","alpha"])
