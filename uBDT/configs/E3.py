928e77ef64eaf97a4ba5995687e5be86bce1a8e9
Kevin Pedro <kpedro88@gmail.com>
2019-06-25 08:21:39 -0500

    configs post-hyper opt

---
+++ uBDT/configs/E3.py
@@ -0,0 +1,6 @@
+from uBDTConfig import uconfig
+
+from E2 import uconfig
+
+uconfig.hyper.min_samples_leaf = 0.01
+uconfig.hyper.max_depth = 4
---
+++ uBDT/configs/E3T.py
@@ -0,0 +1,6 @@
+from uBDTConfig import uconfig
+
+from E2T import uconfig
+
+uconfig.hyper.min_samples_leaf = 0.01
+uconfig.hyper.max_depth = 4
